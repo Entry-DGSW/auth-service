0aebc4ef06b8b39c746a32ef0a45c2829b23521d
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-16 19:40:32 +0900

    Feat: add errorCodes

---
+++ src/main/java/com/sugang/auth/common/errors/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.sugang.auth.common.errors.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    DAUTH_BAD_REQUEST_ERROR(400, "DAUTH-400-1", "Dauth server badRequest error occurred"),
+    DAUTH_UNAUTHORIZED_ERROR(401, "DAUTH-401-1", "Dauth server unauthorized error occurred"),
+    DAUTH_FORBIDDEN_ERROR(403, "DAUTH-403-1", "Dauth server forbidden error occurred"),
+    DAUTH_GONE_ERROR(410, "DAUTH-410-1", "Dauth server gone error occurred"),
+    DAUTH_SERVER_ERROR(500, "DAUTH-500-1", "Dauth server error occurred"),
+
+    BAD_REQUEST_ERROR(400, "AUTH-400-1", "Bad Request error occurred"),
+    INTERNAL_SERVER_ERROR(500, "AUTH-500-1", "Server error occurred");
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
