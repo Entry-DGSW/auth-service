dcbdf35c004fa9b42bd0e0af51bf58445265b01d
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-17 20:00:44 +0900

    Feat: baseExceptions

--- src/main/java/com/sugang/auth/common/errors/exception/BaseException.java
+++ src/main/java/com/sugang/auth/common/errors/exception/BaseException.java
@@ -8,4 +8,16 @@ import lombok.Getter;
 public class BaseException extends RuntimeException {
 
     private final ErrorCode errorCode;
+
+    public int getStatus() {
+        return errorCode.getStatus();
+    }
+
+    public String getCode() {
+        return errorCode.getCode();
+    }
+
+    public String getMessage() {
+        return errorCode.getMessage();
+    }
 }
