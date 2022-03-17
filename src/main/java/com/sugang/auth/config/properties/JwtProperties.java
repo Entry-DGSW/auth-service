f59fc9b054cb3377dcfd2118fdb4d09f924d53c3
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-17 20:01:13 +0900

    Feat: add jwt properties

---
+++ src/main/java/com/sugang/auth/config/properties/JwtProperties.java
@@ -0,0 +1,16 @@
+package com.sugang.auth.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+
+    private String secret;
+    private Long exp;
+}
