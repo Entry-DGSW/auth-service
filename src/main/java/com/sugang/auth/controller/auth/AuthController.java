47fb917cf42089160995415ef37a144b810c3f56
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-17 20:03:43 +0900

    Feat: add controller service

--- src/main/java/com/sugang/auth/controller/auth/AuthController.java
+++ src/main/java/com/sugang/auth/controller/auth/AuthController.java
@@ -1,9 +1,14 @@
 package com.sugang.auth.controller.auth;
 
+import com.sugang.auth.controller.auth.dto.request.AuthRequest;
+import com.sugang.auth.controller.auth.dto.response.TokenResponse;
+import com.sugang.auth.service.auth.DauthAuthService;
 import com.sugang.auth.service.auth.QueryOauthLinkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final QueryOauthLinkService queryOauthLinkService;
+    private final DauthAuthService dauthAuthService;
 
     @GetMapping("dauth")
     public ResponseEntity<String> getDauthQuery() {
         return ResponseEntity.ok(
                 queryOauthLinkService.execute());
+    }
+
+    @PutMapping("dauth")
+    public ResponseEntity<TokenResponse> getDauthTokenResponse(
+            @RequestBody AuthRequest request
+    ) {
+        return ResponseEntity.ok(
+                dauthAuthService.execute(request));
     }
 }
