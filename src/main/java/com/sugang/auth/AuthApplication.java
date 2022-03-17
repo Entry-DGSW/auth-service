9ad7f748d9fd3b13f2c99dfd9c32de4a022d6206
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-17 20:02:09 +0900

    Feat: add kafka source

--- src/main/java/com/sugang/auth/AuthApplication.java
+++ src/main/java/com/sugang/auth/AuthApplication.java
@@ -1,9 +1,12 @@
 package com.sugang.auth;
 
+import com.sugang.auth.common.kafka.UserSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(UserSource.class)
 public class AuthApplication {
 
     public static void main(String[] args) {
