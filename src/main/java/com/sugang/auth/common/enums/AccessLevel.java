023ea8acc7bcc6c9e31be9ab7af0dc9eb2e7b802
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-17 20:01:45 +0900

    Feat: add accessLevel

---
+++ src/main/java/com/sugang/auth/common/enums/AccessLevel.java
@@ -0,0 +1,15 @@
+package com.sugang.auth.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AccessLevel {
+
+    STUDENT(1),
+    TEACHER(2),
+    ADMIN(3);
+
+    private int accessLevel;
+}
